6edd1d013d72b82efe5eb2ea488fdaa192691c76
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2022-03-27 22:40:17 +0200

    bulding new structure of calinder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 import Register from "./components/Register/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { AddTask } from "./components/AddTask/AddTask";
 
 function App() {
   return (
     <div className="App">
       <header className="Todo app">
-        <Register></Register>
+        {/* <Register></Register> */}
+        <AddTask></AddTask>
       </header>
     </div>
   );
